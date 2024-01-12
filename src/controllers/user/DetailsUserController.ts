3e0e84edfa5a28734786f2f11d9ecbe1b526052c
Matheus Alves <matheusfrancisco0330@gmail.com>
2024-01-12 00:29:53 -0300

    feat: user-details

--- src/controllers/user/DetailsUserController.ts
+++ src/controllers/user/DetailsUserController.ts
@@ -3,9 +3,12 @@ import { DetailsUserService } from "../../services";
 
 class DetailsUserController {
     async handle(req: Request, res: Response) {
+        const  user_id  = req.user_id as string;
+
         const detailsUserService = new DetailsUserService();
 
-        const details = await detailsUserService.execute();
+        const details = await detailsUserService.execute(user_id);
+
         return res.json(details);
     }
     ;
--- src/services/user/DetailsUserService.ts
+++ src/services/user/DetailsUserService.ts
@@ -1,9 +1,22 @@
 import primasClient from '../../prisma';
 
 class DetailsUserService {
-    async execute() {
-        return { ok: true };
-    }
+    async execute(user_id: string) {
+
+        const user = await primasClient.prismaClient.user.findFirst({
+            where: {
+                id: user_id
+            },
+            select:{
+                name: true,
+                cpf: true,
+                phone: true,
+                email: true,
+                picture: true,
+            }
+        });
+        return user;
+    };
 };
 
 export { DetailsUserService };
