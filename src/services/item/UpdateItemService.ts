5f473a93dd5ed5a06911ba671a9a061139869d97
Matheus Alves <matheusfrancisco0330@gmail.com>
2024-01-24 09:26:18 -0300

    fix: update item

--- src/services/item/UpdateItemService.ts
+++ src/services/item/UpdateItemService.ts
@@ -1,34 +1,27 @@
-import { Prisma } from '@prisma/client';
 import prismaClient from '../../prisma';
-
 interface IUpdateItem {
-    item_id: string;
-    status_id: string;
+  item_id: string;
+  status_id: string;
 };
-
 class UpdateItemService {
-    async execute(items: IUpdateItem[]) {
-        const updates: Prisma.ItemUpdateManyArgs[] = items.map(item => ({
-            where: {
-                id: item.item_id,
-            },
-            data: {
-                status_id: item.status_id,
-            },
-        }));
+  async execute(items: IUpdateItem[]) {
+    let itemsToReturn = [];
 
-        const itemsToReturn = await prismaClient.prismaClient.item.updateMany({
-            data: {
-                // Combina todos os objetos de dados dos itens em um objeto
-                ...updates.reduce((acc, update) => ({ ...acc, ...update.data }), {}),
-            },
-            where: {
-                OR: updates.map(update => ({ id: update.where.id })),
-            },
-        });
+    for (let i = 0; i < items.length; i++) {
+      const updatedItem = await prismaClient.prismaClient.item.updateMany({
+        where: {
+          id: items[i].item_id,
+        },
+        data: {
+          status_id: items[i].status_id,
+        },
+      });
 
-        return itemsToReturn;
+      itemsToReturn.push(updatedItem);
     }
+
+    return itemsToReturn;
+  }
 };
 
 export { UpdateItemService };
