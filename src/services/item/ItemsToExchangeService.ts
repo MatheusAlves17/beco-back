8b31a23902a707b0799fca060fb6cb60fe62bc34
Matheus Alves <matheusfrancisco0330@gmail.com>
2024-05-12 14:15:51 -0300

    TESTE

--- src/services/item/ItemsToExchangeService.ts
+++ src/services/item/ItemsToExchangeService.ts
@@ -2,6 +2,8 @@ import prismaClient from '../../prisma';
 
 class ItemsToExchangeService {
   async execute(items: string[]) {
+    console.log('OS ITENS PARA TROCA',items);
+    
 
     const status = await prismaClient.prismaClient.status.findFirst({
         where:{
