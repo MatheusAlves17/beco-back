80875f3ede115dfd4f2c6d987105b23e53a192f4
Matheus Alves <matheusfrancisco0330@gmail.com>
2024-05-12 15:55:44 -0300

    fix: response at items-exchange

--- src/services/admin/GetItemsExchangeService.ts
+++ src/services/admin/GetItemsExchangeService.ts
@@ -24,6 +24,7 @@ class GetItemsExchangeService {
                 price: true,
                 banner: true,
                 order_id: true,
+                status: true,
             }
         });
 
