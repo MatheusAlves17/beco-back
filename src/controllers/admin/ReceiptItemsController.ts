661f52312aa4131965e357086fc3d9e7c4458447
Matheus Alves <matheusfrancisco0330@gmail.com>
2024-05-12 23:11:56 -0300

    refactor: add order_id at receipt-items

--- src/controllers/admin/ReceiptItemsController.ts
+++ src/controllers/admin/ReceiptItemsController.ts
@@ -3,10 +3,10 @@ import { ReceiptItemsService } from "../../services";
 
 class ReceiptItemsController {
     async handle(req: Request, res: Response) {
-        const { items } = req.body;
+        const { items, order_id } = req.body;
 
         const receiptItemsService = new ReceiptItemsService();
-        const itemsReceipt = await receiptItemsService.execute(items);
+        const itemsReceipt = await receiptItemsService.execute(items, order_id);
 
         return res.json(itemsReceipt)
     };
