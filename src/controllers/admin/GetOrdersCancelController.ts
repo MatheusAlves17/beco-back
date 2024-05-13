bab581a573456532f519694bddec128321d9fb18
Matheus Alves <matheusfrancisco0330@gmail.com>
2024-05-12 22:27:44 -0300

    refactor

--- src/controllers/admin/GetOrdersCancelController.ts
+++ src/controllers/admin/GetOrdersCancelController.ts
@@ -3,9 +3,10 @@ import { GetOrdersCancelService } from "../../services";
 
 class GetOrdersCancelController {
     async handle(req: Request, res: Response) {
+        const { status_id } = req.body;
 
         const getOrdersCancelService = new GetOrdersCancelService();
-        const orders = await getOrdersCancelService.execute();
+        const orders = await getOrdersCancelService.execute(status_id);
 
         return res.json(orders);
     };
--- src/services/admin/GetOrdersCancelService.ts
+++ src/services/admin/GetOrdersCancelService.ts
@@ -1,17 +1,17 @@
 import prismaClient from '../../prisma';
 
 class GetOrdersCancelService {
-    async execute() {
+    async execute(status_id: string) {
 
-        const status = await prismaClient.prismaClient.status.findFirst({
-            where: {
-                name: "Cancelado"
-            },
-        });
+        // const status = await prismaClient.prismaClient.status.findFirst({
+        //     where: {
+        //         name: "Cancelado"
+        //     },
+        // });
 
         const orders = await prismaClient.prismaClient.order.findMany({
             where: {
-                status_id: status.id
+                status_id: status_id
             }
         });
 
