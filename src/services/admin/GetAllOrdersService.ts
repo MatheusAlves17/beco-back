bd14aa8dcb9ac53dcf056162e14a50cfb725b59d
Matheus Alves <matheusfrancisco0330@gmail.com>
2024-05-12 23:10:43 -0300

    feat: change status when exchange

--- src/services/admin/GetAllOrdersService.ts
+++ src/services/admin/GetAllOrdersService.ts
@@ -3,7 +3,14 @@ class GetAllOrdersService {
     async execute(status_id: string) {
         const orders = await prismaClient.prismaClient.order.findMany({
             where:{
-                status_id: status_id
+                status_id: status_id,
+            },
+            select:{
+                id: true,
+                item: true,
+                status: true,
+                value_total: true,
+                user_id: true,
             }
         });
 
--- src/services/admin/ReceiptItemsService.ts
+++ src/services/admin/ReceiptItemsService.ts
@@ -1,7 +1,7 @@
 import prismaClient from '../../prisma';
 
 class ReceiptItemsService {
-    async execute(items: string[]) {
+    async execute(items: string[], order_id?: string) {
         const status = await prismaClient.prismaClient.status.findFirst({
             where: {
                 name: 'Troca encerrada'
@@ -82,6 +82,15 @@ class ReceiptItemsService {
                 }
             });
         }
+
+        const order = await prismaClient.prismaClient.order.update({
+            where:{
+                id:order_id
+            },
+            data:{
+                status_id: status.id
+            }
+        })
         
         return {msg: 'Estoque atualizado e cupom gerado!'}
 
