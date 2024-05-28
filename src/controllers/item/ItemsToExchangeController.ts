cca953fc580e0d90a26e1bdb0a9f6129f99200ea
Matheus Alves <matheusfrancisco0330@gmail.com>
2024-05-27 22:29:23 -0300

    refactor: update order status

--- src/controllers/item/ItemsToExchangeController.ts
+++ src/controllers/item/ItemsToExchangeController.ts
@@ -3,10 +3,10 @@ import { ItemsToExchangeService } from "../../services";
 
 class ItemsToExchangeController {
     async handle(req: Request, res: Response) {
-        const { items } = req.body;
+        const { items, order_id } = req.body;
         
         const itemsToExchangeService = new ItemsToExchangeService();
-        const itemsToReturn = await itemsToExchangeService.execute(items);
+        const itemsToReturn = await itemsToExchangeService.execute(items, order_id);
 
         return res.json(itemsToReturn);
     };
--- src/services/item/ItemsToExchangeService.ts
+++ src/services/item/ItemsToExchangeService.ts
@@ -1,14 +1,14 @@
 import prismaClient from '../../prisma';
 
 class ItemsToExchangeService {
-  async execute(items: string[]) {
+  async execute(items: string[], order_id: string) {
     const status = await prismaClient.prismaClient.status.findFirst({
-        where:{
-            name: 'Em troca'
-        },
-        select:{
-            id: true
-        }
+      where: {
+        name: 'Em troca'
+      },
+      select: {
+        id: true
+      }
     });
 
     const status_id = status.id
@@ -23,6 +23,17 @@ class ItemsToExchangeService {
         status_id,
       },
     });
+
+    if (updatedItems) {
+      const updateOrder = await prismaClient.prismaClient.order.update({
+        where: {
+          id: order_id
+        },
+        data:{
+          status_id: status.id
+        }
+      })
+    }
 
     return updatedItems;
   }
